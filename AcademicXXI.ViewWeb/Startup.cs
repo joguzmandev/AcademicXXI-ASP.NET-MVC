bf1bda6febf90274c680a69aeab712a515111bde
Jorge Luis Guzmán Saldaña <jorgeguzman2210@gmail.com>
2016-04-21 23:42:45 -0400

    Creando las diferentes capas de la app

---
+++ AcademicXXI.ViewWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AcademicXXI.ViewWeb.Startup))]
+namespace AcademicXXI.ViewWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
