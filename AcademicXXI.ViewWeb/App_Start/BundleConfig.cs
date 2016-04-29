4496fd3c51fe78ba0375578cd060cb35c58a1c22
Jorge Luis Guzmán Saldaña <jorgeguzman2210@gmail.com>
2016-04-29 00:25:50 -0400

    Creando plantilla desde una existen de bootstrap para la Vista

--- AcademicXXI.ViewWeb/App_Start/BundleConfig.cs
+++ AcademicXXI.ViewWeb/App_Start/BundleConfig.cs
@@ -8,8 +8,6 @@ namespace AcademicXXI.ViewWeb
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            //TODO revisar los css y js configurado aqui e integrar metimenu.js
-            //http://mm.onokumus.com/index.html
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
@@ -26,8 +24,7 @@ namespace AcademicXXI.ViewWeb
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/bootstrap.css", new CssRewriteUrlTransform()));
         }
     }
 }
