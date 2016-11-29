3ccf551ae15b6f89d060c6a4b91919072469bd08
Jorge Guzman <jorgeguzman2210@gmail.com>
2016-11-29 15:55:10 -0400

    Add RecordController's method

--- AcademicXXI.Web/Controllers/RecordController.cs
+++ AcademicXXI.Web/Controllers/RecordController.cs
@@ -1,8 +1,14 @@
-﻿using System;
+﻿using AcademicXXI.Services.SemesterService;
+using AcademicXXI.Services.SubjectService;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using AcademicXXI.ViewModel.MapExtensionMethod;
+using vm = AcademicXXI.ViewModel.ViewModel;
+using domian = AcademicXXI.Domain;
 
 namespace AcademicXXI.Web.Controllers
 {
@@ -14,9 +20,30 @@ namespace AcademicXXI.Web.Controllers
             return RedirectToAction("CreateRecord");
         }
 
-        public ActionResult CreateRecord()
+        public async Task<ActionResult> CreateRecord()
         {
+            var result = await _semesterService.GetAllAsync();
+
+
+            
+            SelectList list = new SelectList(result.GenericConvertList<vm.SemesterViewModel>(), "SemesterCode", "SemesterCode");
+            ViewBag.GetAllSemester = list;
             return View();
         }
+
+        public async Task<ActionResult> GetAllSubject()
+        {
+           var result = await _subjectService.GetAllAsync();
+
+            return Json(result.GenericConvertList<vm.SubjectViewModel>(),JsonRequestBehavior.AllowGet);
+        }
+
+        public RecordController(ISubjectService subjectService,ISemesterService semesterService)
+        {
+            this._subjectService = subjectService;
+            this._semesterService = semesterService;
+        }
+        private ISubjectService _subjectService { get; set; }
+        private ISemesterService _semesterService { get; set; }
     }
 }
